330eba9b82d0d9a528adca9cef3b2fc762080309
Alexander Rovang <wolf_vii@yahoo.com>
2021-06-18 11:01:30 -0400

    adds actions to DrawsController

--- app/controllers/draws_controller.rb
+++ app/controllers/draws_controller.rb
@@ -1,2 +1,27 @@
 class DrawsController < ApplicationController
+  def index
+    render({json: Draw.all})
+  end
+
+  def show
+    @draw = Draw.find_by(id: params[:id])
+    render json: @draw
+  end
+
+  def create
+    draw = Draw.create(draw_params)
+    render json: draw
+  end
+
+  def destroy
+    draw = Draw.find_by(id: params[:id])
+    draw.destroy
+    render json: Draw.all
+  end
+
+  private
+
+  def draw_params
+    params.require(:draw).permit(:layout, :card_ids, :orientations)
+  end
 end
