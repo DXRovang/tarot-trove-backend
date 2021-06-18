c012afbf3062bd95414fb2e15054a1e4542b5e61
Alexander Rovang <wolf_vii@yahoo.com>
2021-06-18 11:05:00 -0400

    adds actions to cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,26 @@
 class CardsController < ApplicationController
+
+  def index
+    card = Card.all
+    render json: card
+  end
+
+  def show
+    card = Card.find_by(id: params[:id])
+    render json: card
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(
+      :name,
+      :created_at,
+      :cardType,
+      :summary,
+      :image,
+      :meaning_up,
+      :meaning_rev,
+      :desc
+    )
+  end
 end
